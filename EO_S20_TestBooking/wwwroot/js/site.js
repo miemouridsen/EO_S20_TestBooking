ee94e57ea478d5cad87aeec5b96d4e4813e010f7
miemouridsen <miemouridsen@hotmail.com>
2021-12-17 21:31:50 +0100

    js added

--- EO_S20_TestBooking/wwwroot/js/site.js
+++ EO_S20_TestBooking/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var func = function () {
+    document.getElementById("times").hidden = true;
+}();
+
+document.getElementById("dateButton").addEventListener(function (event) {
+    document.getElementById("times").hidden = false;
+});
